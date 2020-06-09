8876c79c209b9a324ea5bfee71b9488635916e70
JonathanHelvey <jonathanhelvey27@gmail.com>
2020-06-08 23:42:41 -0400

    netlify fix

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../themes/themes.css';
 
-import Header from '../components/Header/Header';
+import Header from '../components/Header';
 import GasChecker from '../components/GasChecker.js';
 import Footer from '../components/Footer';
 
